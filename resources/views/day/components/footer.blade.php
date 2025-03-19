fd68b82e0c005492edfb3e115742f9b3a0ff0645
Jadson Silva <jadsonpe@hotmail.com>
2025-03-19 19:00:04 -0300

    [FIX]Ajuste footer

--- resources/views/day/components/footer.blade.php
+++ resources/views/day/components/footer.blade.php
@@ -8,7 +8,7 @@
             <div class="footer-contact pt-3">
               {{-- <p>A108 Adam Street</p>
               <p>New York, NY 535022</p> --}}
-              <p class="mt-3"><strong>Cel/Whatsapp:</strong> <span>+1 5589 55488 55</span></p>
+              <p class="mt-3"><strong>Cel/Whatsapp:</strong> <span>+55 81996581977</span></p>
               <p><strong>Email:</strong> <span>info@example.com</span></p>
             </div>
             <div class="social-links d-flex mt-2">
