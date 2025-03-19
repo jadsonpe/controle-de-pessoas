623548d588d93388ab3307c7f6e4927d9223be21
Jadson Silva <jadsonpe@hotmail.com>
2025-03-19 19:44:36 -0300

    Rotas web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CondominioController;
+use App\Http\Controllers\ApartamentoController;
+use App\Http\Controllers\EntradaSaidaController;
+
+Route::get('/', function () {
+    return redirect()->route('condominios.index');
+});
+
+Route::resource('condominios', CondominioController::class);
+Route::resource('apartamentos', ApartamentoController::class);
+Route::resource('entradas', EntradaSaidaController::class);
+Route::post('entradas/{id}/checkout', [EntradaSaidaController::class, 'checkout'])->name('entradas.checkout');
+
 
 Route::get('/', function () {
     return view('day.index');
@@ -9,3 +23,13 @@ Route::get('/', function () {
 Route::get('/portifolio', function () {
     return view('day.portifolio-detail');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
+
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.process');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
